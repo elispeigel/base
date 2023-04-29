c51b77538794841ef26aa5572ef7403833a9ae1c
Eli Speigel <eli.speigel@gmail.com>
2023-04-28 19:20:39 -0700

    test: add thermostat test

---
+++ internal/devices/thermostat/thermostat_test.go
@@ -0,0 +1,52 @@
+package thermostat
+
+import (
+	"base/internal/devices"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"go.uber.org/zap/zaptest"
+)
+
+func TestSmarThermostat(t *testing.T) {
+	logger = zaptest.NewLogger(t)
+
+	thermostat := SmartThermostat{
+		ID:          "123",
+		Name:        "Test Thermostat",
+		IsOn:        false,
+		Temperature: 50,
+	}
+
+	// Test TurnOn
+	err := thermostat.TurnOn()
+	assert.NoError(t, err)
+	assert.True(t, thermostat.IsOn)
+
+	// Test TurnOff
+	err = thermostat.TurnOff()
+	assert.NoError(t, err)
+	assert.False(t, thermostat.IsOn)
+
+	// Test GetStatus
+	status, err := thermostat.GetStatus()
+	assert.NoError(t, err)
+	expectedStatus := devices.Status{
+		DeviceID:   "123",
+		DeviceName: "Test Thermostat",
+		IsOn:       false,
+	}
+	assert.Equal(t, expectedStatus, status)
+
+	// Test GetTemperature
+	assert.Equal(t, 50, thermostat.GetTemperature())
+
+	// Test SetTemperature
+	err = thermostat.SetTemperature(80)
+	assert.NoError(t, err)
+	assert.Equal(t, 80, thermostat.GetTemperature())
+
+	// Test SetTemperature with invalid value
+	err = thermostat.SetTemperature(-1)
+	assert.Error(t, err)
+}
